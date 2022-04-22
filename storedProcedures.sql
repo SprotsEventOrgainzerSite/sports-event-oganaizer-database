723d1ef3f232f170d42f59f497a9e6c0de6acbc7
Varga Balázs <vargabalazs900513@mgail.com>
2022-04-22 19:13:30 +0200

    Adatlekérdező tárolt eljárások megírása

--- storedProcedures.sql
+++ storedProcedures.sql
@@ -6,6 +6,42 @@ BEGIN
     return loggedCount = 1;
 END
 
+DROP FUNCTION IF EXISTS listUsers;
+CREATE FUNCTION listUsers() RETURNS JSON
+BEGIN
+	DECLARE userList JSON;
+    SELECT CONCAT("[", GROUP_CONCAT( JSON_OBJEcT('id' , id,'Név' , userName)), "]" )  INTO userList from UserTable;
+    RETURN userList;
+END
+
+
+DROP FUNCTION IF EXISTS listEvents;
+CREATE FUNCTION listEvents(timeFrom TIMESTAMP, timeTo TIMESTAMP) RETURNS JSON
+BEGIN
+	DECLARE eventList JSON;
+	SELECT CONCAT("[", GROUP_CONCAT( JSON_OBJECT('id' , id,'title' , title, 'startTime', startTime, 'regTime', regTime, 'category', category) ), "]" ) into eventList from Events
+    WHERE (timeFrom is NULL OR timeFrom < startTime)AND(timeTo IS NULL OR timeTo > startTime);
+    RETURN eventList;
+END
+
+DROP FUNCTION IF EXISTS eventDetails;
+CREATE FUNCTION eventDetails(eventid INT) RETURNS JSON
+BEGIN
+	DECLARE eventD JSON;
+	SELECT CONCAT("[", GROUP_CONCAT( JSON_OBJECT('id' , id,'title' , title, 'startTime', startTime, 'regTime', regTime, 'category', category) ), "]" ) into eventD from Events
+    WHERE eventiD = id;
+    RETURN eventD;
+END
+
+DROP FUNCTION IF EXISTS userDetails;
+CREATE FUNCTION userDetails(userid INT) RETURNS JSON
+BEGIN
+	DECLARE userD JSON;
+	SELECT CONCAT("[", GROUP_CONCAT( JSON_OBJECT('id' , id,'Név' , userName, 'mail', mail) ), "]" ) into userD from UserTable
+    WHERE userid = id;
+    RETURN userD;
+END
+
 DROP PROCEDURE IF EXISTS registerToEvent;
 CREATE PROCEDURE registerToEvent(IN userId VARCHAR(6), eventId INT) 
 BEGIN
